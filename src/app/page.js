bc0829cbdd17ad7ff548b527834b1c3df923fe01
martjellino <mryanmarcellino@gmail.com>
2023-10-18 19:44:59 +0700

    finalize

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,4 @@
 // "use client"
-import { Page1 } from "@/components/Page1";
 import { Page2 } from "@/components/Page2";
 import { Page3 } from "@/components/Page3";
 import { Page4 } from "@/components/Page4";
